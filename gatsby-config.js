1ab0504e99010611b68d1ec53e835c20b54b8316
deepinwine <jiangjia0907@gmail.com>
2024-01-17 21:39:55 +0800

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,8 +3,8 @@
 module.exports = {
   pathPrefix: process.env.PATH_PREFIX || '/',
   siteMetadata: {
-    siteTitle: 'Running Page',
-    siteUrl: 'https://jiangjia.run',
+    siteTitle: '修仙记录',
+    siteUrl: 'https://deepinwine.github.io/running_page/',
     logo: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTtc69JxHNcmN1ETpMUX4dozAgAN6iPjWalQ&usqp=CAU',
     description: 'Personal site and blog',
     navLinks: [
